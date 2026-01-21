8a66c9ec6a58e95cfbc10aa7b507a7475445a32a
ANSH <ansh310.sharma@gmail.com>
2026-01-22 00:09:45 +0530

    add username uniqueness check API and dotenv dependency

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,60 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import {z} from "zod";
+
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { use } from "react";
+
+
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request) {
+    await dbConnect();
+
+    try {
+        const { searchParams } = new URL(request.url);
+        const queryParams = {
+            username: searchParams.get("username") 
+        }
+
+        const result = UsernameQuerySchema.safeParse(queryParams);
+
+        if (!result.success) {
+
+            return Response.json({
+                success: false,
+                message: "Invalid username format.",
+            }, { status: 400 }
+            )
+        }
+        const { username } = result.data;
+
+        const existingVerifiedUser = await UserModel.findOne({
+            username: username,
+            isVerified: true
+        });
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: "Username is already taken.",
+            }, { status: 200
+            })
+        }
+        return Response.json({
+            success: true,
+            message: "Username is available.",
+        }, { status: 200
+        })
+    } catch (error) {
+        console.error("Error checking username uniqueness:", error);
+        return Response.json({
+            success: false,
+            message: "Internal server error.",
+        }, { status: 500
+        })
+    }
+}
+
+
